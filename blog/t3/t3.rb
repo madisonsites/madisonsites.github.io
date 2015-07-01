7c68b43fa69a824fa8ee78202b3314c71415a052
MadisonSites <madison.sites@gmail.com>
2015-06-30 22:08:03 -0700

    add t3 blog

---
+++ blog/t3/t3.rb
@@ -0,0 +1,26 @@
+
+class_of_2008 = ["Madison Sites", "Kelly Munson", "Regina George"]
+
+
+
+Varsity_jocks = ["Shane Oman", "Aaron Samuels"]
+
+the_greatest_people_you_will_ever_meet = ["Janice Ian", "Damian Leigh", "Cady Heron"]
+
+and_the_worst = ["Regina George", "Gretchen Wieners", "Karen Smith"]
+
+>>class_of_2008 = Varsity_jocks + the_greatest_people_you_will_ever_meet + and_the_worst
+
+north_shore_gpas = {
+  'Janice Ian' => 3.5
+  'Damian Leigh' => 3.7
+  'Cady Heron' => 3.8
+  'Regina George' => 3.0
+  'Gretchen Wieners' => 2.8
+  'Karen Smith' => -.5
+  'crying girl' => "she doesn't even go here"
+}
+
+
+
+
